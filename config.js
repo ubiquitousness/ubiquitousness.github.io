c9a7fb6c23a5549e86dc7240c28a11dbbe858b30
ubiquitousness <168612396+ubiquitousness@users.noreply.github.com>
2025-02-09 18:06:37 +0900

    Update config.js

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ window.MathJax = {
   tex: {
     tags: "ams",
     inlineMath: [['$', '$'], ['\(', '\)']],
-    packages: {'[+]': ['physics']}
+    packages: {'[+]': ['physics2']}
   },
-  loader: {load: ['[tex]/physics']},
+  loader: {load: ['[tex]/physics2']},
   chtml: {
     mtextInheritFont: true
   }
