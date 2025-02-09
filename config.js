768eabe2df7b53d9d16edacebba355cd1e92295d
ubiquitousness <168612396+ubiquitousness@users.noreply.github.com>
2025-02-09 17:46:35 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,7 @@
 window.MathJax = {
 tex: {
-tags: "ams",
-macros: {
-x: "{\\times}",
-bm: ["{\\boldsymbol{#1}}",1],
-dd: ["{\\frac{\\partial #1}{\\partial #2}}",2]
-}
+  tags: "ams",
+  packages: {'[+]': ['physics']}
 },
 chtml: {
 mtextInheritFont: true
