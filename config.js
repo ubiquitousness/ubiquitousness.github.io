71bdc1d1daa8fbbdcc34c9c18cb6118e0978970a
ubiquitousness <168612396+ubiquitousness@users.noreply.github.com>
2025-02-09 16:27:18 +0900

    Create config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+window.MathJax = {
+tex: {
+tags: "ams",
+macros: {
+x: "{\\times}",
+bm: ["{\\boldsymbol{#1}}",1],
+dd: ["{\\frac{\\partial #1}{\\partial #2}}",2]
+}
+},
+chtml: {
+mtextInheritFont: true
+}
+}; 
